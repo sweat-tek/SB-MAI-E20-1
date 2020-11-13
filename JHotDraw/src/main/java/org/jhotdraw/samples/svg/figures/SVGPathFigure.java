e22e95dff527521392344b260bcd3ac660d0b5b2
SofieLouise <sofma18@student.sdu.dk>
2020-11-13 10:25:28 +0100

    cleaned code

--- JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
+++ JHotDraw/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
@@ -5,15 +5,16 @@
  * and all its contributors.
  * All rights reserved.
  *
- * The copyright of this software is owned by the authors and  
- * contributors of the JHotDraw project ("the copyright holders").  
- * You may not use, copy or modify this software, except in  
- * accordance with the license agreement you entered into with  
- * the copyright holders. For details see accompanying license terms. 
+ * The copyright of this software is owned by the authors and
+ * contributors of the JHotDraw project ("the copyright holders").
+ * You may not use, copy or modify this software, except in
+ * accordance with the license agreement you entered into with
+ * the copyright holders. For details see accompanying license terms.
  */
 package org.jhotdraw.samples.svg.figures;
 
 import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
@@ -22,12 +23,14 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.undo.*;
+
 import org.jhotdraw.app.JHotDrawFeatures;
 import org.jhotdraw.draw.*;
 import org.jhotdraw.geom.*;
 import org.jhotdraw.samples.svg.*;
 import org.jhotdraw.util.*;
 import org.jhotdraw.xml.*;
+
 import static org.jhotdraw.samples.svg.SVGAttributeKeys.*;
 
 /**
@@ -38,32 +41,37 @@ import static org.jhotdraw.samples.svg.SVGAttributeKeys.*;
  * @version 2.1 2009-04-17 Method contains() takes now into account
  * whether the figure is filled.
  * <br>1.1.1 2008-03-20 Attributes must be set on child figures in order
- * to ensure that the drawing area of the child figures is computed properly. 
- * <br>1.1 2007-12-21 Only close/open last path. 
+ * to ensure that the drawing area of the child figures is computed properly.
+ * <br>1.1 2007-12-21 Only close/open last path.
  * <br>1.0 July 8, 2006 Created.
  */
 public class SVGPathFigure extends AbstractAttributedCompositeFigure implements SVGFigure {
 
+    private final static boolean DEBUG = false;
+    // private transient Rectangle2D.Double cachedDrawingArea;
     /**
      * This cachedPath is used for drawing.
      */
     private transient GeneralPath cachedPath;
-   // private transient Rectangle2D.Double cachedDrawingArea;
     /**
      * This is used to perform faster hit testing.
      */
     private transient Shape cachedHitShape;
-    private final static boolean DEBUG = false;
 
-    /** Creates a new instance. */
+    /**
+     * Creates a new instance.
+     */
     @FeatureEntryPoint(JHotDrawFeatures.LINE_TOOL)
     public SVGPathFigure() {
         add(new SVGBezierFigure());
         SVGAttributeKeys.setDefaults(this);
     }
+
     @FeatureEntryPoint(JHotDrawFeatures.LINE_TOOL)
     public SVGPathFigure(boolean isEmpty) {
-        if (! isEmpty) { add(new SVGBezierFigure()); }
+        if (!isEmpty) {
+            add(new SVGBezierFigure());
+        }
         SVGAttributeKeys.setDefaults(this);
     }
 
@@ -127,7 +135,7 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
     }
 
     protected void drawChildren(Graphics2D g) {
-    // empty
+        // empty
     }
 
     public void drawFill(Graphics2D g) {
@@ -138,7 +146,8 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
         g.draw(getPath());
     }
 
-    @Override protected void invalidate() {
+    @Override
+    protected void invalidate() {
         super.invalidate();
         cachedPath = null;
         cachedDrawingArea = null;
@@ -156,18 +165,18 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
         }
         return cachedPath;
     }
+
     protected Shape getHitShape() {
         if (cachedHitShape == null) {
             cachedHitShape = getPath();
             if (FILL_COLOR.get(this) == null && FILL_GRADIENT.get(this) == null) {
                 cachedHitShape = SVGAttributeKeys.getHitStroke(this).createStrokedShape(cachedHitShape);
-                }
+            }
 
         }
         return cachedHitShape;
     }
 
-    
     // int count;
     public Rectangle2D.Double getDrawingArea() {
         if (cachedDrawingArea == null) {
@@ -209,18 +218,18 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
             p = getInverseTransform(p);
         }
         boolean isClosed = CLOSED.get(getChild(0));
-        if (isClosed && FILL_COLOR.get(this) == null && FILL_GRADIENT.get(this)==null) {
+        if (isClosed && FILL_COLOR.get(this) == null && FILL_GRADIENT.get(this) == null) {
             return getHitShape().contains(p);
         }
         double tolerance = Math.max(2f, AttributeKeys.getStrokeTotalWidth(this) / 2d);
-        if (isClosed || FILL_COLOR.get(this) != null || FILL_GRADIENT.get(this)!=null) {
+        if (isClosed || FILL_COLOR.get(this) != null || FILL_GRADIENT.get(this) != null) {
             if (getPath().contains(p)) {
                 return true;
             }
             double grow = AttributeKeys.getPerpendicularHitGrowth(this);
             GrowStroke gs = new GrowStroke((float) grow,
                     (float) (AttributeKeys.getStrokeTotalWidth(this) *
-                    STROKE_MITER_LIMIT.get(this)));
+                            STROKE_MITER_LIMIT.get(this)));
             if (gs.createStrokedShape(getPath()).contains(p)) {
                 return true;
             } else {
@@ -287,7 +296,7 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
         invalidate();
         Object[] restoreData = (Object[]) geometry;
         ArrayList<Object> paths = (ArrayList<Object>) restoreData[0];
-        for (int i = 0,  n = getChildCount(); i < n; i++) {
+        for (int i = 0, n = getChildCount(); i < n; i++) {
             getChild(i).restoreTransformTo(paths.get(i));
         }
         TRANSFORM.basicSetClone(this, (AffineTransform) restoreData[1]);
@@ -298,15 +307,15 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
     @Override
     public Object getTransformRestoreData() {
         ArrayList<Object> paths = new ArrayList<Object>(getChildCount());
-        for (int i = 0,  n = getChildCount(); i < n; i++) {
+        for (int i = 0, n = getChildCount(); i < n; i++) {
             paths.add(getChild(i).getTransformRestoreData());
         }
-        return new Object[] {
-            paths,
-            TRANSFORM.getClone(this),
-            FILL_GRADIENT.getClone(this),
-            STROKE_GRADIENT.getClone(this)   
-        };   
+        return new Object[]{
+                paths,
+                TRANSFORM.getClone(this),
+                FILL_GRADIENT.getClone(this),
+                STROKE_GRADIENT.getClone(this)
+        };
     }
 
     @Override
@@ -314,7 +323,7 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
         super.setAttribute(key, newValue);
         invalidate();
     }
-    
+
     public boolean isEmpty() {
         for (Figure child : getChildren()) {
             SVGBezierFigure b = (SVGBezierFigure) child;
@@ -329,7 +338,7 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
     public Collection<Handle> createHandles(int detailLevel) {
         LinkedList<Handle> handles = new LinkedList<Handle>();
         switch (detailLevel % 2) {
-            case -1 : // Mouse hover handles
+            case -1: // Mouse hover handles
                 handles.add(new SVGPathOutlineHandle(this, true));
                 break;
             case 0:
@@ -352,7 +361,6 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
     public Collection<Action> getActions(Point2D.Double p) {
         final ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
         LinkedList<Action> actions = new LinkedList<Action>();
-        assert TRANSFORM.get(this) != null;
         if (TRANSFORM.get(this) != null) {
             actions.add(createRemoveTransformAction(labels));
             actions.add(createFlattenTransformAction(labels));
