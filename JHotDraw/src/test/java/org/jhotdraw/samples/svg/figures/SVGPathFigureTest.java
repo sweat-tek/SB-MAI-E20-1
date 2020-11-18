f0053566e52da343f6f38493317d3eae9daf0dbc
SofieLouise <sofma18@student.sdu.dk>
2020-11-18 11:29:34 +0100

    Cleaned test class

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
@@ -18,7 +18,6 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
@@ -27,64 +26,51 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 /**
- *
  * @author slm
  */
 public class SVGPathFigureTest {
     SVGPathFigure svgPathFigure;
-    Point2D.Double point;
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+    Point2D.Double point = new Point2D.Double(1, 1);
+
     @Before
     public void setUp() {
         svgPathFigure = new SVGPathFigure();
-        point = new Point2D.Double(1, 1);
-    }
-    
-    @After
-    public void tearDown() {
     }
 
     @Test
     public void testConstructor() {
-        SVGPathFigure svgPathFigure1 = new SVGPathFigure();
-        // Assert that the constructor adds an SVGBezierFigure object to the list of children.
-        assertFalse(svgPathFigure1.getChildren().isEmpty());
-        assertSame(svgPathFigure1.getChildren().get(0).getClass(), SVGBezierFigure.class);
+        assertFalse(svgPathFigure.getChildren().isEmpty());
+        assertSame(svgPathFigure.getChildren().get(0).getClass(), SVGBezierFigure.class);
+        assertSame(AttributeKeys.WINDING_RULE.get(svgPathFigure), AttributeKeys.WindingRule.NON_ZERO);
 
         SVGPathFigure svgPathFigureEmpty = new SVGPathFigure(true);
-        //Assert that the other constructor doesn't add a figure to the list of children
         assertTrue(svgPathFigureEmpty.getChildren().isEmpty());
-        //Assert that the default values have been set correctly on both SVGPathFigures
-        assertSame(AttributeKeys.WINDING_RULE.get(svgPathFigure1), AttributeKeys.WindingRule.NON_ZERO);
         assertSame(AttributeKeys.WINDING_RULE.get(svgPathFigureEmpty), AttributeKeys.WindingRule.NON_ZERO);
     }
 
     @Test
-    public void testGetActions() {
-        Collection<Action> actions = svgPathFigure.getActions(point);
-        assertFalse(actions.isEmpty());
-        assertEquals(2, actions.size());
-
-        for (String label : Arrays.asList("Close Path", "Even Odd")) {
-            assertTrue(actions.stream().anyMatch(action -> action.getValue(Action.NAME).equals(label)));
-        }
+    public void testGetActionsDefault() {
+        assertActions(svgPathFigure.getActions(point), 2,"Close Path", "Even Odd");
     }
 
     @Test
     public void testGetActionsWithTransformAttribute() {
         svgPathFigure.setAttribute(AttributeKeys.TRANSFORM, new AffineTransform());
-        Collection<Action> actions = svgPathFigure.getActions(point);
-        assertEquals(4, actions.size());
+        assertActions(svgPathFigure.getActions(point), 4,
+                "Remove Transformation", "Flatten Transformation", "Close Path", "Even Odd");
+    }
 
-        for (String label : Arrays.asList("Remove Transformation", "Flatten Transformation", "Close Path", "Even Odd")) {
+    @Test
+    public void testGetActionsClosedAndEvenOdd() {
+        svgPathFigure.getChild(svgPathFigure.getChildCount() - 1)
+                .setAttribute(AttributeKeys.CLOSED,true);
+        svgPathFigure.setAttribute(AttributeKeys.WINDING_RULE, AttributeKeys.WindingRule.EVEN_ODD);
+        assertActions(svgPathFigure.getActions(point),2,"Open Path", "Non Zero");
+    }
+
+    private void assertActions(Collection<Action> actions, int numberOfActions, String... labels) {
+        assertEquals(numberOfActions, actions.size());
+        for (String label : labels) {
             assertTrue(actions.stream().anyMatch(action -> action.getValue(Action.NAME).equals(label)));
         }
     }
