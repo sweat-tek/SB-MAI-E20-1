38d34a6de3247624a59751bd6d1630515cf629ee
Hoang Oscar Dao <43139435+oscardao@users.noreply.github.com>
2020-11-19 20:43:19 +0100

    added unit test

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGAttributedFigureTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jhotdraw.samples.svg.figures;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.util.Collection;
+import javax.swing.Action;
+import org.jhotdraw.xml.DOMInput;
+import org.jhotdraw.xml.DOMOutput;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author oscar
+ */
+public class SVGAttributedFigureTest {
+
+    public SVGAttributedFigureTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of draw method, of class SVGAttributedFigure.
+     */
+    @Test
+    public void testDraw() {
+
+    }
+
+}
