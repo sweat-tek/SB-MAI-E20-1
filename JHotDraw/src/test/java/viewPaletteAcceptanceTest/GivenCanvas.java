40148029a9660cb98331f6a3e2ed032c27906a72
Emilie Karlsson <Emilie97k@gmail.com>
2020-11-20 10:55:38 +0100

    .

--- JHotDraw/src/test/java/viewPaletteAcceptanceTest/GivenCanvas.java
+++ JHotDraw/src/test/java/viewPaletteAcceptanceTest/GivenCanvas.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package viewPaletteAcceptanceTest;
+
+
 
 /**
  *
  * @author emili
  */
 public class GivenCanvas {
+    
+    @ProvidedScenarioState
+    DefaultDrawingEditor editor;
+    
+    @BeforeStage
+    private void before() {
+        
+    }
     
 }
--- JHotDraw/src/test/java/viewPaletteAcceptanceTest/viewPaletteAcceptanceTest.java
+++ JHotDraw/src/test/java/viewPaletteAcceptanceTest/viewPaletteAcceptanceTest.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package viewPaletteAcceptanceTest;
+
+import org.junit.Test;
+
 
 /**
  *
  * @author emili
  */
 public class viewPaletteAcceptanceTest {
+    
+    @Test
+    public void vewPaletteAcceptanceTest(){
+        //As an end user I want to be able to add a grid to the canvas as well as zoom in and out,
+        //so that it will be easier to see and align objects if needed.
+        
+        given().aCanvasIsCreated();
+    }
     
 }
