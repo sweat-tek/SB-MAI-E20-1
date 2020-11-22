f65c5a3e5bf47576272383ccd0502d0ea635c323
Emilie Karlsson <Emilie97k@gmail.com>
2020-11-22 17:51:49 +0100

    change file to capital

--- JHotDraw/src/test/java/viewPaletteAcceptanceTest/ZoomInAcceptanceTest.java
+++ JHotDraw/src/test/java/viewPaletteAcceptanceTest/ZoomInAcceptanceTest.java
@@ -7,13 +7,12 @@ import com.tngtech.jgiven.junit.ScenarioTest;
  *
  * @author emili
  */
-public class ZoomAcceptanceTest extends ScenarioTest<GivenCanvas, WhenZoom, ThenZoom> {
+public class ZoomInAcceptanceTest extends ScenarioTest<GivenCanvas, WhenZoom, ThenZoom> {
     
     @Test
     public void ZoomAcceptanceTest(){
         //As an end user I want to be able to zoom in and out,
         //so that it will be easier to see objects if needed.
-        
         given().aViewIsCreated();
         when().zoomIsChanged();
         then().zoomHappens();
